ce8f0fa53873247eb1f36d2c280acc5346984a89
Pedro Reis <dropecosta@gmail.com>
2025-05-07 18:39:24 +0100

    First commit. Install NextJS

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  
+
+  return (
+    <>
+      <Head><title>My Beer Collection</title></Head>
+      <main>
+        <h1>My Beer Collection</h1>
+      </main>
+    </>
+  )
+}
