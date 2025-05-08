e71540c3711786d9dff70c19ca01caa8d4550a38
Pedro Reis <dropecosta@gmail.com>
2025-05-09 00:12:54 +0100

    Removing dependencies

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("244f6d63ada312ab")
+/******/ 		__webpack_require__.h = () => ("e262e0756a2a3d8d")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
