cf65859cf39a9da3c78b22f1d55529e948108851
Pedro Reis <dropecosta@gmail.com>
2025-05-09 12:33:25 +0100

    Fixing bugs from Vercel deployment details build - 6

--- app/beer/[id]/page.tsx
+++ app/beer/[id]/page.tsx
@@ -1,14 +1,7 @@
 import { getBeerById } from '@/lib/api'
 import BeerDetail from '@/components/BeerDetail/BeerDetail'
 
-export default async function BeerDetailPage({
-  params,
-  searchParams,
-}: {
-  params: { id: string }
-  searchParams: Record<string, string | string[] | undefined>
-}) {
-  const { id } = params;
-  const beer = await getBeerById(id);
+export default async function BeerDetailPage({ params }: { params: { id: string } }) {
+  const beer = await getBeerById(params.id);
   return <BeerDetail beer={beer} />;
 }
